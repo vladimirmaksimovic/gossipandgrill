053b0e23918585944372f54dce19018dc2a6c201
VM <vmaksimovic.cr34@gmail.com>
2022-09-09 13:19:33 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// style color of navigation link
+const navLink = document.querySelectorAll(".nav-link");
+
+for (let i = 0; i < navLink.length; i++) {
+  if (navLink[i].attributes[0].value === "#") {
+    navLink[i].style.color = "var(--blue)";
+  }
+}
